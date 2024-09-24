175acc267e45d99a36ced8e169ccd1a4d551b0fd
Likhitha Potturu <66682406+LP2006@users.noreply.github.com>
2024-09-24 09:35:25 +0000

    modified requirements

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,42 +1,11 @@
 import streamlit as st
-import pyodbc
 
-# Function to establish a connection to the PostgreSQL database
-@st.cache_resource
-def connect_to_db():
-    try:
-        # Connection string using the correct ODBC driver
-        conn_str = (
-            "DRIVER={PostgreSQL ODBC Driver(UNICODE)};"
-            "SERVER=172.26.63.252;"
-            "PORT=5432;"
-            "DATABASE=postgres;"  
-            "UID=lkp;"      
-            "PWD=voeko;"      
-        )
+# Initialize connection.
+conn = st.connection("postgresql", type="sql")
 
-        # Establish the connection
-        conn = pyodbc.connect(conn_str)
-        st.success("Connection successful!")
-        return conn
+# Perform query.
+df = conn.query('SELECT * FROM Site;', ttl="10m")
 
-    except pyodbc.Error as e:
-        st.error(f"Error connecting to database: {e}")
-        return None
-
-# Streamlit app layout
-st.title("PostgreSQL ODBC Connection in Streamlit")
-
-# Button to trigger connection attempt
-if st.button("Connect to Database"):
-    conn = connect_to_db()
-    
-    if conn:
-        # Example of querying the database if connection is successful
-        query = "SELECT version();"
-        cursor = conn.cursor()
-        cursor.execute(query)
-        version = cursor.fetchone()
-        st.write(f"PostgreSQL version: {version[0]}")
-        cursor.close()
-        conn.close()
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.SiteID} has a :{row.SiteName}:")
